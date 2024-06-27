c151a0037c69f2fadab166661757fd74cdde6359
Ellrnn <rannamead58@gmail.com>
2024-06-27 11:43:35 -0300

    first challenge

--- src/Modules/hero/useCases/update/validator.cs
+++ src/Modules/hero/useCases/update/validator.cs
@@ -11,5 +11,9 @@ public class Validator : Validator<Request>
         RuleFor(request => request.name)
             .NotEmpty()
             .WithMessage("Parameter \"name\" is required!");
+
+        RuleFor(request => request.description)
+            .NotEmpty()
+            .WithMessage("Parameter \"description\" is required!");
     }
 }
